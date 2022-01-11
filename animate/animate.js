618312b71727d57a9a0b744d76a50f8dbdfd5652
KrutikaBhatt <krutikabhatt222@gmail.com>
2022-01-12 00:50:23 +0530

    Added the styles to canvas

--- animate/animate.js
+++ animate/animate.js
@@ -3,7 +3,11 @@
 const stars = document.getElementById('stars');
 const starsCtx = stars.getContext('2d');
 const slider = document.querySelector(".slider");
-
+document.body.style.zoom = 1.0
+var scale = 'scale(1)';
+document.body.style.webkitTransform =  scale;    // Chrome, Opera, Safari
+document.body.style.msTransform =   scale;       // IE 9
+document.body.style.transform = scale;     // General
 
 // global variables
 let screen, starsElements, starsParams = { speed: 2, number: 650, extinction: 4 };
@@ -16,7 +20,7 @@ updateStars();
 
 slider.onmouseover = function() {
     console.log("mouse")
-    starsParams.speed = 18;
+    starsParams.speed = 20;
 };
 
 slider.onmouseout = function() {
@@ -53,7 +57,7 @@ function Star() {
 
         starsCtx.beginPath();
         starsCtx.fillStyle = "rgba(255, 255, 255, " + opacity + ")";
-        starsCtx.arc(x, y, rad-0.8, 0, Math.PI * 2);
+        starsCtx.arc(x, y, rad-0.2, 0, Math.PI * 2);
         starsCtx.fill();
     }
 }
@@ -82,5 +86,6 @@ function updateStars() {
         s.show();
         s.move();
     });
+
     window.requestAnimationFrame(updateStars);
 }
